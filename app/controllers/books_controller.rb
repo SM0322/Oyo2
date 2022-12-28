e86e4b8bd4f71c10a0f8a9ff712f7d4a251e7f5b
EC2 Default User <ec2-user@ip-172-31-9-127.ap-northeast-1.compute.internal>
2022-12-28 04:32:14 +0000

    Initial Commit Kansei

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,21 +1,24 @@
 class BooksController < ApplicationController
+before_action :ensure_current_book, {only: [:edit, :update]}
+
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
 
   def show
     @book = Book.find(params[:id])
-  end
-
-  def index
-    @books = Book.all
+    @books = Book.new
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
+      redirect_to book_path(@book.id), notice: "You have created book successfully."
     else
       @books = Book.all
-      render 'index'
+      render :index
     end
   end
 
@@ -32,15 +35,22 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
+  
+  def ensure_current_book
+   @book = Book.find(params[:id])
+   if current_user.id != @book.user_id
+     redirect_to books_path
+   end
+  end 
 end
